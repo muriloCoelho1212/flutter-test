67802fd415ea3349299fa11b48972f0d1f18c099
Murilo Coelho <m.coelhos1212@gmail.com>
2024-04-14 21:15:55 -0300

    feat: add new button in project

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A random AWESOME teste idea:'),
           Text(appState.current.asLowerCase),
+
+          ElevatedButton(onPressed: () {
+            print('Button pressed');
+          }, child: Text('Next'))
         ],
       ),
     );
